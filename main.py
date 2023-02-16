ec76056f800db981d5f0bccf05a17a40b8c50125
Crazyplopl <crazyplopl17@gmail.com>
2023-02-16 09:58:53 +0000

    Update main.py
    - Fixed bug not letting you move left and up at the same time
    - Added support for non-integer movements
    - Freed memory by deleting variables that are no longer required

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import turtle
 import os
 import math    
 
-if not os.path.exists("import.VECTOR"):
+if os.path.exists("import.VECTOR"):
     with open("import.VECTOR", "r") as f: 
-        exec(f"points={f.read().strip()}")
+        exec(f"points={f.readline().strip()}")
 else:
     print("Custom points not found: Attempting to load default")
     if os.path.exists("default.VECTOR"):
@@ -15,48 +15,56 @@ else:
         print("Default points not found: Stopping...")
         input()
         exit()
-print(points)
+
 if os.path.exists("import.CONF"):
     with open("import.CONF", "r") as f:
         settingsRaw = f.readlines()
     settings = []
     for setting in settingsRaw:
-        settings.append(int(setting))
+        settings.append(float(setting))
+    del setting
+    del settingsRaw
 else:
     settings = [5,5,5]
     print("Settings not found: Loaded defaults")
 
 vectors = []
-for x in range(0, len(points)):
-    points[x][1] *= settings[1]
-    points[x][0] *= settings[1]
-    if int(points[x][1]) == 0 and int(points[x][0]) == 0:
+for point in points:
+    x = float(point[0]) * settings[1]
+    y = float(point[1]) * settings[1]
+    if y == 0 and x == 0:
         angle = 0
         magnitude = 0
-    elif int(points[x][0]) == 0 or int(points[x][1]) == 0:
-        angle = 0
+    elif x == 0 and y != 0:
+        if y < 0:
+            angle = -90
+        else:
+            angle = 90
+    elif x != 0 and y == 0:
+        if x < 0:
+            angle = 180
+        else:
+            angle = 0
+    elif x < 0 and y > 0:
+        angle = 180 + math.degrees(math.atan(y / x))
     else:
-        angle = math.degrees(math.atan(int(points[x][1]) / int(points[x][0])))
-    magnitude = math.sqrt((int(points[x][0]) * int(points[x][0])) + (int(points[x][1]) * int(points[x][1])))
-    if angle == 0.0:
-        angle = 0
-    if magnitude == 0.0:
-        magnitude = 0
-    if angle == 0 and points[x][0] < 0:
-        magnitude *= -1
-    elif angle == 0 and points[x][1] < 0:
-        angle = -90
-    elif angle == 0 and points[x][1] > 0:
-        angle = 90
+        angle = math.degrees(math.atan(y / x))
+    magnitude = math.sqrt((x**2) + (y**2))
     vectors.append([angle, magnitude])
+    del angle
+    del magnitude
+    del x
+    del y
+del point
+del points
 
 turtle.hideturtle()
 turtle.speed(settings[0])
 turtle.width(settings[2])
+del settings
 
 pendown = True
 turtle.pendown()
-
 for x in range(0, len(vectors)):
     if vectors[x] == [0,0] and not pendown:
         turtle.pendown()
